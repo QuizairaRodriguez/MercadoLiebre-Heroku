21ffa5e5bf78ddeffb6db5ad8576a51a6c0450fa
QuizairaRodriguez <kiss02andre@gmail.com>
2022-02-17 11:13:55 -0300

    Modificación de puerto y creación del Procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const pathPublic = path.resolve(__dirname,'public');
 app.use( express.static( pathPublic ) );
 
-app.listen( 3000, () => {
+app.listen( process.env.PORT || 3000 , function() {
     console.log('Servidor funcionando');
 });
 
